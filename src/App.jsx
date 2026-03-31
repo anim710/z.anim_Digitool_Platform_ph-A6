7f68ee66b518b3961a3d017058678b79c317b722
anim710 <anim3654@gmail.com>
2026-04-01 01:51:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+
+          <h1 className='text-red-500'>hello world</h1>
+         
+    </>
+  )
+}
+
+export default App
